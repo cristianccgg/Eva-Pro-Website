938604bf984ca5297de65daf4dd28f478dff0da7
cristianccgg <cristianccggg@gmail.com>
2024-10-21 18:38:20 -0500

    Landing in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         "secondary-black": "#282828",
         gray: "#454545",
         "light-gray": "#828282",
+        "gray-bg": "linear-gradient(62% #F2F2F6 0%, 44% #ECECEC 100%)",
       },
     },
     plugins: [],
